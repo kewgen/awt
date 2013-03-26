3a4999307646cc4e6fc754a872f262c7a31c28de
mikhail.kutuzov <mikhail.kutuzov.geargames@gmail.com>
2013-03-26 18:44:50 +0400

    0. исправил ошибку правописания 1.испрвлена ошибка отписки от рассылки 2.подписчики теперь оповещаются о событиях с наибольшей частотой

--- common/com/geargames/common/network/MessageDispatcher.java
+++ common/com/geargames/common/network/MessageDispatcher.java
@@ -10,11 +10,14 @@ import com.geargames.common.util.ArrayObject;
  * Date: 25.03.13
  * Класс описывает регистрацию слушателей сообщений сети и оповещает их о приходе сообщений.
  * После доставки сообщений из Network подписчикам происходит их удаление из Network.
+ * Рассылка происходит с наименьшим требуемым подписчиками периодом(каждый подписчик возвращает этот желаемый параметр).
  */
-public class MessageDispetcher implements TimerListener {
-    private ArrayObject messageListeners;
+public class MessageDispatcher implements TimerListener {
+    private ArrayList listeners;
+    private ArrayObject listenersByType;
     private Network network;
     private int id;
+    private int period;
 
     /**
      * Класс создаётся по экземпляру Network из которого будут вычитываться сообщения и количеству типов сообщений
@@ -23,46 +26,66 @@ public class MessageDispetcher implements TimerListener {
      * @param network
      * @param size
      */
-    public MessageDispetcher(Network network, int size) {
+    public MessageDispatcher(Network network, int size) {
         this.network = network;
-        this.messageListeners = new ArrayObject(size);
-        id = TimerManager.setPeriodicTimer(10000, this);
+        this.listenersByType = new ArrayObject(size);
+        this.listeners = new ArrayList();
     }
 
     /**
      * Подписаться на получение сообщений из Network по интерфейсу DataMessageListener.
+     *
      * @param listener
      */
     public void register(DataMessageListener listener) {
+        if (listeners.isEmpty()) {
+            period = listener.getInterval();
+            id = TimerManager.setPeriodicTimer(period, this);
+        } else if (period > listener.getInterval()) {
+            period = listener.getInterval();
+            TimerManager.setPeriodicTimer(id, period, this);
+        }
+
+        listeners.add(listener);
         short[] types = listener.getTypes();
         int length = types.length;
         for (int i = 0; i < length; i++) {
             int type = types[i];
-            ArrayList typeListeners = (ArrayList) messageListeners.get(type);
-            if (messageListeners.get(type) == null) {
+            ArrayList typeListeners = (ArrayList) listenersByType.get(type);
+            if (typeListeners == null) {
                 typeListeners = new ArrayList();
-                messageListeners.set(type, typeListeners);
+                listenersByType.set(type, typeListeners);
             }
             typeListeners.add(listener);
         }
     }
 
     /**
-     * Сменить период опроса Network о сообщениях.
-     * @param period в миллисекундах
-     */
-    public void changePeriod(int period){
-        TimerManager.setPeriodicTimer(id, period, this);
-    }
-
-    /**
      * Отписаться на получение сообщений из Network по интерфейсу DataMessageListener.
+     *
      * @param listener
      */
     public void unregister(DataMessageListener listener) {
-        int size = messageListeners.length();
+        int size = listenersByType.length();
         for (int i = 0; i < size; i++) {
-            ((ArrayList) messageListeners.get(0)).remove(listener);
+            ((ArrayList) listenersByType.get(i)).remove(listener);
+        }
+        listeners.remove(listener);
+
+        if (listener.getInterval() == period) {
+            size = listeners.size();
+            if (size > 0) {
+                period = ((DataMessageListener)listeners.get(0)).getInterval();
+                for (int i = 1; i < size; i++) {
+                    int time = ((DataMessageListener) listeners.get(i)).getInterval();
+                    if (period > time) {
+                        period = time;
+                    }
+                }
+                TimerManager.setPeriodicTimer(id, period, this);
+            } else {
+                TimerManager.killTimer(id);
+            }
         }
     }
 
@@ -71,7 +94,7 @@ public class MessageDispetcher implements TimerListener {
         DataMessage[] messages = network.getAsynchronousDataMessages();
         for (int i = 0; i < messages.length; i++) {
             DataMessage message = messages[i];
-            ArrayList listeners = (ArrayList) messageListeners.get(message.getMessageType());
+            ArrayList listeners = (ArrayList) listenersByType.get(message.getMessageType());
             int size = listeners.size();
             for (int j = 0; j < size; j++) {
                 ((DataMessageListener) listeners.get(j)).onReceive(message);
